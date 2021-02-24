b7c1d6eaee3d7678fd9cd0d4d1e7534ea907ba68
hatem <hatem.bayoudh1@esprit.tn>
2021-02-24 23:37:52 +0100

    first

---
+++ src/main/java/tn/esprit/spring/PiDev4Sae6Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiDev4Sae6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiDev4Sae6Application.class, args);
+	}
+
+}
