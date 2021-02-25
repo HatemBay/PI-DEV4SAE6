3faa921e9c4415d40b15e3c6a3640925754bcc2f
raef <raef.bchiri1@esprit.tn>
2021-02-25 14:10:06 +0100

    Entities commit

--- src/main/java/tn/esprit/spring/forniture/entities/Forniture.java
+++ src/main/java/tn/esprit/spring/forniture/entities/Forniture.java
@@ -1,5 +1,59 @@
 package tn.esprit.spring.forniture.entities;
 
-public class Forniture {
+import java.io.Serializable;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="Forniture")
+public class Forniture implements Serializable {
+
+	 @Id
+	 @GeneratedValue (strategy = GenerationType.IDENTITY)
+	  private int id ;
+	  private String description;
+	  private String photo;
+	  private int telephone ;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public int getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(int telephone) {
+		this.telephone = telephone;
+	}
+	
+	public Forniture(){}
+	
+	public Forniture(int id, String description, String photo, int telephone) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.photo = photo;
+		this.telephone = telephone;
+	}
+	@Override
+	public String toString() {
+		return "Forniture [id=" + id + ", description=" + description + ", photo=" + photo + ", telephone=" + telephone
+				+ "]";
+	}
+	  
+	  
 }
--- src/main/java/tn/esprit/spring/forniture/entities/Type_forniture.java
+++ src/main/java/tn/esprit/spring/forniture/entities/Type_forniture.java
@@ -1,5 +1,49 @@
 package tn.esprit.spring.forniture.entities;
 
-public class Type_forniture {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Type_Forniture")
+public class Type_forniture implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String type;
+	
+	
+	public Type_forniture(){}
+	
+	public Type_forniture(int id, String type) {
+		super();
+		this.id = id;
+		this.type = type;}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Type_forniture [id=" + id + ", type=" + type + "]";
+	}
+	
+	
 }
